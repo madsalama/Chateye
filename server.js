45edaa0d6ae8ff9e5b7880120c2e5da90d24569b
Salama <mahmoud.ali.salama@hpe.com>
2017-04-01 17:57:02 +0200

    commit

--- server.js
+++ server.js
@@ -465,8 +465,59 @@ reqs.end();
   else if (messageAttachments) { 
     if (messageAttachments.type="image")
   {
-      mvision.detect(senderID, timeOfMessage, fs, request, visionClient, messageAttachments[0].payload.url);
-      console.log(faceresults); 
+      var image = messageAttachments[0].payload.url;
+
+      // download the image & access it!
+            var download = function(uri, filename, callback){
+            request.head(uri, function(err, res, body){
+
+            console.log('content-type:', res.headers['content-type']);
+            console.log('content-length:', res.headers['content-length']);
+
+            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);});
+            };
+
+            download(image, './static/'+''+senderID+'_'+timeOfMessage+'.jpg', function(){
+            console.log('image downloaded!');
+            var image_path = './static/'+''+senderID+'_'+timeOfMessage+'.jpg';
+
+            visionClient.detectFaces(image_path)
+            .then((results) => {
+                const faces = results[0];
+
+                console.log('Faces:');
+                faces.forEach((face, i) => {
+
+                    faceresults = {
+
+                "confidence":JSON.stringify(faces[i].confidence),   
+
+                "joy":JSON.stringify(faces[i].joy),
+                "joyLikelihood":JSON.stringify(faces[i].joyLikelihood),
+
+                "sorrow":JSON.stringify(faces[i].sorrow),
+                "sorrowLikelihood":JSON.stringify(faces[i].sorrowLikelihood),
+
+                "anger":JSON.stringify(faces[i].anger),
+                "angerLikelihood":JSON.stringify(faces[i].angerLikelihood),
+
+                "surprise":JSON.stringify(faces[i].surprise),
+                "surpriseLikelihood":JSON.stringify(faces[i].surpriseLikelihood),
+
+                "underExposed":JSON.stringify(faces[i].underExposed),
+                "underExposedLikelihood":JSON.stringify(faces[i].underExposedLikelihood),
+
+                "blurred":JSON.stringify(faces[i].blurred),
+                "blurredLikelihood":JSON.stringify(faces[i].blurredLikelihood),
+
+                "headwear":JSON.stringify(faces[i].headwear),
+                "headwearLikelihood":JSON.stringify(faces[i].headwearLikelihood)
+            };                            
+            });
+            });
+            });
+
+      console.log(faceresults);
 
   }
 
