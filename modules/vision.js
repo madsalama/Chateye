4fa59a3b25ad25dd93b60d4acc6883a9bc62e071
Salama <mahmoud.ali.salama@hpe.com>
2017-04-01 12:51:05 +0200

    commit

--- modules/vision.js
+++ modules/vision.js
@@ -5,8 +5,10 @@ module.exports = {
 
    visionClient.detectFaces(image, function(err, faces) {
     
-        console.log(faces);
+        
         var faces = JSON.stringify(faces);
+        var err = JSON.stringify(err); 
+        
         console.log(faces);
         console.log(err);
 
