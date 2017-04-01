84c2d3b47d530b754a459ceba85e0cbff1bc1d47
Salama <mahmoud.ali.salama@hpe.com>
2017-04-01 13:28:34 +0200

    commit

--- modules/vision.js
+++ modules/vision.js
@@ -11,31 +11,37 @@ module.exports = {
             console.log('content-length:', res.headers['content-length']);
 
             request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-                console.log(JSON.stringify(err));
                 
             });
             };
 
             download(image, './static/'+''+senderID+'_'+timeOfMessage+'.jpg', function(){
             console.log('image downloaded!');
+
+
+            var image_path = '../static/'+''+senderID+'_'+timeOfMessage+'.jpg'; 
+
+            visionClient.detectFaces(image_path, function(err, faces) {
+
+                var faces = JSON.stringify(faces);
+                var err = JSON.stringify(err); 
+        
+                console.log(faces);
+                console.log(err);
+
+            // delete the image
+
+        }); 
+
+
         });
         
 
-        var image_path = '../static/'+''+senderID+'_'+timeOfMessage+'.jpg'; 
-
-        visionClient.detectFaces(image_path, function(err, faces) {
-
-            var faces = JSON.stringify(faces);
-            var err = JSON.stringify(err); 
-        
-            console.log(faces);
-            console.log(err);
-
-            // delete the image. 
-
         
 
-        }); 
+
+
+
 
 
   
