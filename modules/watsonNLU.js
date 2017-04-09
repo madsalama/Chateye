c7fa04e5140cb130b46175f844d5bc9b4c8e8849
mahmoud <mahmoud>
2017-04-09 10:26:25 +0200

    commit

--- modules/watsonNLU.js
+++ modules/watsonNLU.js
@@ -14,16 +14,21 @@ module.exports = {
             // 'targets': [
             //   'apples',
             //   'oranges' ]
-
+        },
+        'sentiment':{
+            // 'targets': [
+            //   'apples',
+            //   'oranges' ]
         }
+
     }
             }, function(err, response) {
                 if (err)
                 console.log('error:', err);
                 else
                 {
-                    console.log(JSON.stringify(response, null, 2));
-                    callback(module.exports.returnData(response));
+                    // console.log(JSON.stringify(response, null, 2));
+                    callback(module.exports.returnData(JSON.stringify(response, null, 2)));
                 }
                 
             });
