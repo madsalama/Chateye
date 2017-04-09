e99118d85a0bd4d168a7ed27df2a003858c09588
mahmoud <mahmoud>
2017-04-09 11:41:03 +0200

    commit

--- modules/audio.js
+++ modules/audio.js
@@ -29,7 +29,7 @@ module.exports={
                         for (var file in files) {
                         var params = {
                             audio: fs.createReadStream(files[file]),
-                            content_type: 'audio/l16; rate=8000; channels=1',
+                            content_type: 'audio/l16',
                             timestamps: true,
                             // word_alternatives_threshold: 0.9,
                             // keywords: ['colorado', 'tornado', 'tornadoes'],
