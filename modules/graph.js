d45c51c5a9d392efde378d97dda2cdb4b91d9211
mahmoud <mahmoud>
2017-04-12 07:20:01 +0200

    commit

--- modules/graph.js
+++ modules/graph.js
@@ -13,12 +13,16 @@ module.exports = {
 
     options = {    
             uri: 'https://graph.facebook.com/v2.8/'+userID+'/likes',
-            method: 'GET',            
+            method: 'GET',   
+            headers:module.exports.headers,         
             qs: { access_token: 'EAAXdsmtZAx2oBAElkgercsynCvZCqOpoC34wffTFgboGO4j5h02kmmy4SiJ1ayBjcvQ8A2r40JUvn9hptnZCuen9A6t7xoYIcff6Yj3xuckHlZCLPhe2O9S44xRSFSQhL0b82unbVO63NNH1fu1EVDhJ2X51GSpFCzXUytDNOgZDZD' }
     }; 
 
     request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {                          
+        if (!error && response.statusCode == 200) {       
+                console.log(data);
+                console.log(JSON.parse(data));
+
                 callback(module.exports.returnData(body));
         }
     });
