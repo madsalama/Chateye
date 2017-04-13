3f64bace88c5a3b9eab56b2590db9883156a19c8
Salama <mahmoud.ali.salama@hpe.com>
2017-04-13 11:19:23 +0200

    commit

--- server.js
+++ server.js
@@ -477,7 +477,7 @@ function getAction(senderID) {
 function getLanguageName(langlist, language) {
    for (var i in langlist) {
      if (langlist[i].alpha2 == language) {        
-        return users[i].English;
+        return langlist[i].English;
      }
    }
 };
