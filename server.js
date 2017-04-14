8067c06f8200f1fdf87227a7009e8c505ff0ced3
Salama <mahmoud.ali.salama@hpe.com>
2017-04-14 11:51:20 +0200

    commit

--- server.js
+++ server.js
@@ -147,30 +147,25 @@ var App = function() {
 // ====================================
 //     LOCAL START UP TESTS GO HERE 
 // ====================================
-
-
-
+// 
+//
+//
 // ====================================
 
 
 var addEntry = function(db, callback) {
 
-  var collection = db.collection('entries');
-
-  collection.insertOne([    
-    {
-      entryID : 1,
-      entryContents: '7amada is coool !!'
-    }
+   // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
   ], function(err, result) {
-    
     assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    assert.equal(1, result.ops.length);
-    console.log("Inserted 1 document into the collection");
-
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
     callback(result);
-
   });
 
 }
