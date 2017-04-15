6582150bf3db3ce4edd46b7d5942652dc7a4a73d
mahmoud <mahmoud>
2017-04-15 10:50:05 +0200

    commit

--- modules/graph.js
+++ modules/graph.js
@@ -4,11 +4,6 @@ module.exports = {
         return data;                
     },
 
-    headers:{
-            'User-Agent':       'Super Agent/0.0.1',
-            'Content-Type':     'application/x-www-form-urlencoded', 
-        },
-
 createGetStarted:function(request, callback){
 
         var message = {
