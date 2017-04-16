fe128b15b088915fcdb92b5a2f66b739547b9d50
mahmoud <mahmoud>
2017-04-16 18:03:07 +0200

    commit

--- server.js
+++ server.js
@@ -310,6 +310,7 @@ elements.push(
             // image_url: "",    // image defined according to note mood 
             "buttons":[
               { type:"postback",
+                title: "view", 
                 payload:"show_note_number_"+n
               }]          
 
