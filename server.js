233f93f6f0f26d716eeee2fe827fea5a6584fb80
mahmoud <mahmoud>
2017-04-17 08:52:52 +0200

    commit

--- server.js
+++ server.js
@@ -479,7 +479,7 @@ if (response.result.action === 'save-entry') {
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+var dateTime = date+' | '+time;
 
 
 var entry = getEntry(senderID);
