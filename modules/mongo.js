560a9c47f60a253186bece158b05db4572d0dcd5
mahmoud <mahmoud>
2017-04-19 10:22:26 +0200

    commit

--- modules/mongo.js
+++ modules/mongo.js
@@ -126,12 +126,12 @@ getUserLocale: function(MongoClient, assert, db_url, userID, callback){
                         users.update({ _id: userID },
                         { $pull: { user_entries: { _id: entryID }}}, function(err, result){
                                     console.log(" === MONGO LOGGER USERS UPDATE === ");
-                                    err?console.log(err):console.log(JSON.stringify(result.opts.documents));
+                                    err?console.log(err):console.log(result.opts.documents);
 
                                     
                                 entries.deleteOne( { _id : entryID }, function(err, result){
                                     console.log(" === MONGO LOGGER ENTRY DELETE === ");
-                                    err?console.log(err):console.log(JSON.stringify(result.opts.documents));                                    
+                                    err?console.log(err):console.log(result.opts.documents);
 
                                     // result?
                                     // callback(module.exports.returnData(result))
