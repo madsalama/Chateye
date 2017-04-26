8de5779ce6079389422ffc5aa347b62de2fd51db
Salama <mahmoud.ali.salama@hpe.com>
2017-04-26 17:50:57 +0200

    commit

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ var options = {
     method: 'POST',
     json: messageData
     
-    ,proxy: 'http://proxy:9954c1da29e3-4792-865b-96f4e4004f0d@proxy-54-235-72-96.proximo.io'
+    // ,proxy: 'http://proxy:9954c1da29e3-4792-865b-96f4e4004f0d@proxy-54-235-72-96.proximo.io'
 
 };
 
