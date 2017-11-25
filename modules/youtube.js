ec9a591f9dd4188420edb6c2cc9b20183dad2fe9
mahmoud <mahmoud>
2017-11-25 14:29:12 +0200

    commit

--- modules/youtube.js
+++ modules/youtube.js
@@ -5,11 +5,15 @@ module.exports = {
     },
 
 getVideo: function(request, keywords, limit, callback){
-        var keywords = encodeURIComponent(keywords);        
+        var keywords = encodeURIComponent(keywords);  
+        var headers = {'Referrer': 'https://chatzer.herokuapp.com'};
+
         var options = {
                 url: 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCRWF1EfsvVlXTSLsG6XWir2KuRyPtkBB8&part=snippet\
 &q='+keywords+'&order=viewCount&maxResults='+limit,
-                method: 'GET' }; 
+                method: 'GET',
+                headers: headers
+            }; 
 
                 request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
