02981ffb0bf14c7f21295fdd7f8a1cd7d5a6a28e
Salama <mahmoud.ali.salama@hpe.com>
2018-01-24 18:58:32 +0200

    commit

--- modules/kairos.js
+++ modules/kairos.js
@@ -4,7 +4,8 @@
 module.exports = {
     headers:{
             'User-Agent':       'Super Agent/0.0.1',
-            'Content-Type':     'application/x-www-form-urlencoded',
+            // 'Content-Type':     'application/x-www-form-urlencoded',
+            'Content-Type':'application/json',
             'app_id': 'b1592ea1',
             'app_key': 'ddfb3317a291a021d1ee240b54f517cf' },
 
@@ -39,9 +40,7 @@ module.exports = {
             options = {
                 url: 'https://api.kairos.com/detect',
                 method: 'POST',
-                json: {
-                    "image":"http://chatzer.herokuapp.com/"+senderID+"_"+timeOfMessage+"_kairos.jpg",
-                    "selector":"ROLL" },                    
+                body: "{ \"image\":\"http://chatzer.herokuapp.com/\"+senderID+\"_\"+timeOfMessage+\"_kairos.jpg\",\"selector\":\"ROLL\"}", 
                 headers: module.exports.headers 
             }; 
 
