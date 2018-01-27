05eb5c4889877a26407039f97b34f47aa0271ba5
mahmoud <mahmoud_salama8086@yahoo.com>
2018-01-27 15:47:29 +0200

    commit

--- prettify.sh
+++ prettify.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-jsfiles=$(git diff --cached --name-only --diff-filter=ACM "*.js" "*.jsx" | tr '\n' ' ')
-[ -z "$jsfiles" ] && exit 0
+
+./node_modules/.bin/prettier --write "{*.js,modules/*.js}"
+mv server-pretty.js server.js
+
+#jsfiles=$(git diff --cached --name-only --diff-filter=ACM "*.js" "*.jsx" | tr '\n' ' ')
+#[ -z "$jsfiles" ] && exit 0
 
 # Prettify all staged .js files
-echo "$jsfiles" | xargs ./node_modules/.bin/prettier --write
+#echo "$jsfiles" | xargs ./node_modules/.bin/prettier --write
 
 # Add back the modified/prettified files to staging
-echo "$jsfiles" | xargs git add
+#echo "$jsfiles" | xargs git add
 
-exit 0
+#exit 0
